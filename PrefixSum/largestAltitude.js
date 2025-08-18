00592244746a2d9fc2c2edb1c29f209795db3f35
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-08-18 14:44:39 -0700

    largestAltitude

---
+++ PrefixSum/largestAltitude.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    let currPoint = 0;
+    let highestAltitude = 0;
+    //console.log(currPoint);
+    for(let i = 0; i < gain.length; i++){
+        highestAltitude = Math.max(highestAltitude, currPoint + gain[i]);
+        currPoint = currPoint + gain[i];
+        //console.log(currPoint);
+    }
+    return highestAltitude;
+};
