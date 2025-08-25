51960e8e177f9b59fdf6cf3faf97d195fbc82015
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-08-24 19:48:40 -0700

    searchBST

---
+++ BinarySearchTree/searchBST.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function (root, val) {
+  let curr = root;
+  while (curr) {
+    if (curr.val === val) return curr;
+    else if (curr.val < val) curr = curr.right;
+    else if (val < curr.val) curr = curr.left;
+  }
+  return null;
+};
