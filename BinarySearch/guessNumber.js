d8c068534f14d215e7666c0629751629c6042af5
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-08-24 23:39:13 -0700

    guessNumber

---
+++ BinarySearch/guessNumber.js
@@ -0,0 +1,28 @@
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function (n) {
+  let start = 1;
+  let end = n;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    const res = guess(mid);
+
+    if (res === 0) return mid;
+    else if (res === -1) end = mid - 1;
+    else if (res === 1) start = mid + 1;
+  }
+
+  return null;
+};
