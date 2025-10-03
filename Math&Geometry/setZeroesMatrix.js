869beae6ece3c4279f2e6a8f1f653787614e129c
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-10-02 21:38:51 -0700

    setZeroesMatrix

---
+++ Math&Geometry/setZeroesMatrix.js
@@ -0,0 +1,53 @@
+class Solution {
+  /**
+   * @param {number[][]} matrix
+   * @return {void}
+   */
+  setZeroes(matrix) {
+    const m = matrix.length;
+    if (m === 0) return;
+    const n = matrix[0].length;
+
+    let firstColHasZero = false;
+    let firstRowHasZero = false;
+
+    for (let i = 0; i < m; i++) {
+      if (matrix[i][0] === 0) {
+        firstColHasZero = true;
+        break;
+      }
+    }
+
+    for (let j = 0; j < n; j++) {
+      if (matrix[0][j] === 0) {
+        firstRowHasZero = true;
+        break;
+      }
+    }
+
+    for (let i = 1; i < m; i++) {
+      for (let j = 1; j < n; j++) {
+        if (matrix[i][j] === 0) {
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
+        }
+      }
+    }
+
+    for (let i = 1; i < m; i++) {
+      for (let j = 1; j < n; j++) {
+        if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+
+    if (firstColHasZero) {
+      for (let i = 0; i < m; i++) matrix[i][0] = 0;
+    }
+
+    if (firstRowHasZero) {
+      for (let j = 0; j < n; j++) matrix[0][j] = 0;
+    }
+  }
+}
