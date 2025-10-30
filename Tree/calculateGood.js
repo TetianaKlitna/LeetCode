3b1c876d2be4fc9101f9a666dac11dbdf98fd635
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-10-30 16:11:57 -0700

    goodNodes

---
+++ Tree/calculateGood.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+  /**
+   * @param {TreeNode} root
+   * @return {number}
+   */
+  goodNodes(root) {
+    if (!root) return 0;
+
+    let cnt = 0;
+
+    const calculateGood = (node, max) => {
+      if (!node) return 0;
+
+      if (node.val >= max) {
+        cnt++;
+        max = node.val;
+      }
+      console.log(node.val, max, cnt);
+
+      calculateGood(node.left, max);
+      calculateGood(node.right, max);
+    };
+
+    calculateGood(root, root.val);
+
+    return cnt;
+  }
+}
